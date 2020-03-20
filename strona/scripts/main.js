4787bd57a8d635e574ff832cd0acd04b9c7dd054
Krzysztof Kampa <krzysztofk7777@gmail.com>
2020-03-20 19:10:34 +0100

    Website update

--- strona/scripts/main.js
+++ strona/scripts/main.js
@@ -1,21 +1,27 @@
-debugger;
-var number1 = 0;
-var number2 = number1 + 4;
-var wasUserLogged = false;
-
-
-for (var i = 0; i < 5; i++) {
-    console.log(number1);
-}
-for (var i = 0; i < 5; i++) {
-    console.log("Wykonałem się "+i+" razy");
-}
-
-var colors = {
-    red: "red",
-    blue: "blue",
-    white: "white",
-}
-
 var element = window.document.getElementById("whatever");
 element.innerHTML = "Coś innego niż cokolwiek";
+
+function nowyBobr() {
+    var col = document.getElementById("whatever");
+    var btn = document.createElement("button");
+    btn.innerText = "Nowy bobr";
+    col.appendChild(btn);
+  }
+
+  var i = 1;
+
+  function dodajInformacje() {
+    var nazwaRamki = "informacja" + i;
+    try {
+      var ramka = document.getElementById(nazwaRamki);
+      console.log(ramka.innerHTML);
+      ramka.innerHTML = "Informacja nr "+ i;
+      throw " jakis blad";
+    } catch(err) {
+      console.log("Wystapil blad!");
+      var inf = document.createElement("button");
+      inf.setAttribute('id', nazwaRamki);
+      var informacje = document.getElementById("informacje");
+      informacje.appendChild(inf);
+    }
+  }
